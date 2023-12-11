9ab4b9b96cc90a23b2cc01a0af409f002522f643
kylestarrett1 <kylestarrett1@gmail.com>
2023-12-10 18:41:41 -0600

    Added toggler for sidebar.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import SidebarHeader from './Layout/SidebarHeader';
 import SidebarContent from './Layout/SidebarContent';
@@ -8,19 +8,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
+  const sidebarRef = useRef(null);
   const [addCollapsed, setAddCollapsed] = useState('');
 
   const toggleCollapse = (e) => {
     if (addCollapsed === '') {
       setAddCollapsed('collapsed');
       e.target.classList.add('collapsed');
+      sidebarRef.current.style.width = 8 + 'rem';
     } else {
       setAddCollapsed('');
+      sidebarRef.current.style.width = 'initial';
     }
   };
 
   return (
-    <div className="layout has-sidebar fixed-sidebar fixed-header">
+    <div
+      className="layout has-sidebar fixed-sidebar fixed-header"
+      ref={sidebarRef}
+    >
       <aside id="sidebar" className={`sidebar break-point-sm ${addCollapsed}`}>
         <a
           id="btn-collapse"
